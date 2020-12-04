e039989c1f3b6c2450eebe6eb8c7dfa52089b078
zoeturin <53230753+zoeturin@users.noreply.github.com>
2020-12-04 12:52:33 -0700

    added additional service calls

--- simple_control/scripts/go_forward_controller.py
+++ simple_control/scripts/go_forward_controller.py
@@ -2,52 +2,68 @@
 
 import rospy
 from std_msgs.msg import String
-# from controller import Robot, Camera
+#from controller import Robot, Camera
 # from vehicle import Driver, Car
 import numpy as np
 from webots_ros.srv import *
+#from simple_control.srv import *
 
+class service(object):
+    def __init__(self, path, srv_type):
+        self.path = vehicle_name + path
+        self.type = srv_type
+        rospy.loginfo("Waiting for service %s" %self.path)
+        rospy.wait_for_service(self.path)
+        self.srv = rospy.ServiceProxy(self.path, self.type)
+        rospy.loginfo("Got service %s" %self.path)
 
 def callback(data):
     global vehicle_name
     vehicle_name = data.data
     rospy.loginfo("Got vehicle name")
 
+
 def main():
-
-    # #robot.__init__();
-    # front_camera.enable(30)
-    # rear_camera.enable(30)
-    # lidar.enable(timestep);
-    # lidar.enablePointCloud();
-    # print("lidar freq: "+str(lidar.getFrequency()));
-    #
-    # #robot.setSteeringAngle(0.5);
-    # robot.setThrottle(1);
-    # robot.setBrakeIntensity(0);
-    # robot.setGear(1); # starts in 0 (neutral)
-
-    # Main loop:
-    # - perform simulation steps until Webots is stopping the controller
     while vehicle_name is None:
         pass
-    rospy.loginfo("Waiting for services")
-    rospy.loginfo("Vehicle name:" + vehicle_name)
-    rospy.wait_for_service(vehicle_name + "/automobile/set_throttle")
-    rospy.wait_for_service(vehicle_name + "/automobile/set_gear")
-    rospy.loginfo("Got services")
+    rospy.loginfo("Vehicle name: " + vehicle_name)
+
+    # Services
+    # Control
+    set_throttle = service("/automobile/set_throttle", set_float)
+    set_brake_intensity = service("/automobile/set_brake_intensity", set_float)
+    set_gear = service("/automobile/set_gear", set_int)
+    set_steering_angle = service("/automobile/set_steering_angle", set_float)
+    # Misc
+    get_dimensions = service("/automobile/get_dimensions", automobile_get_dimensions)
+    get_time_step = service("/robot/get_basic_time_step", get_float)
+    # Sensors
+    enable_front_camera = service("/front_camera/enable", set_int)
+    enable_rear_camera = service("/rear_camera/enable", set_int)
+    enable_lidar = service("/Sick_LMS_291/enable", set_int)
+    enable_point_cloud = service("/Sick_LMS_291/enable_point_cloud", set_bool)
+    enable_gps = service("/gps/enable", set_int)
+
+    rospy.loginfo("All services ready")
+
+    # Initial service calls
+    # Control
+    set_brake_intensity.srv(0)
+    set_gear.srv(1)
+    # Misc
+    dims = get_dimensions.srv()
+    timestep = get_time_step.srv()
+    # Sensors
+    enable_front_camera.srv(30)
+    #enable_lidar.srv(timestep)
+    enable_point_cloud.srv(True)
+
+
     while True: #robot.step() != -1:
         # pc = lidar.getPointCloud();
-        try:
-            gear_srv = rospy.ServiceProxy(vehicle_name+'/automobile/set_gear', set_int)
-            throttle_srv = rospy.ServiceProxy(vehicle_name+'/automobile/set_throttle', set_float)
-            gear_srv(1)
-            throttle_srv(1)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        try
+        set_throttle.srv(1)
 
-    # Enter here exit cleanup code.
-    #robot.__del__();
 
 
 # create the Robot instance.
