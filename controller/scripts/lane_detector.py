98458d86e0e46837a936794bcef2d0ae1d35c25d
Zoe Turin <zoeturin@gmail.com>
2020-12-09 23:28:06 -0700

    added controller node for lane detection

--- controller/scripts/lane_detector.py
+++ controller/scripts/lane_detector.py
@@ -10,7 +10,7 @@ def detect_edges(frame):
     2. Adjust gamma value to make lane lines stand out more
     3. Applies Gaussian filter to smooth out sharp edges
     4. Find edges using Canny Edge detection
-    
+
     @ param frame: image from front camera
     @ returns: image with lane line edges found
     '''
@@ -37,7 +37,7 @@ def region_of_interest(frame):
     left_top = [0, height * 0.875] # define left top coordinate point
     right_top = [width, height * 0.875] # define right top coordinate point
     vertices = np.array([[left_bottom, left_top, right_top, right_bottom]], dtype=np.int32)
-    
+
     if len(mask.shape) == 2:
         cv2.fillPoly(mask, vertices, 255)
     else:
@@ -49,11 +49,11 @@ def gaussian_smoothing(frame, kernel_size = 5):
     """
     Apply Gaussian filter to the input image. Called by detect_edges.
     Modified from: https://github.com/dctian/DeepPiCar/blob/master/driver/code/hand_coded_lane_follower.py
-    
+
     @param image: An np.array compatible with plt.imshow
     @param kernel_size (Default = 13): The size of the Gaussian kernel will affect the performance of the detector.
                                        It must be an odd number (3, 5, 7, ...)
-    @returns: image with applied Gaussian filter. Needed for better edge detection. 
+    @returns: image with applied Gaussian filter. Needed for better edge detection.
     """
     return cv2.GaussianBlur(frame, (kernel_size, kernel_size), 0)
 
@@ -69,7 +69,7 @@ def adjust_gamma(frame, gamma=0.30):
     invGamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255
         for i in np.arange(0, 256)]).astype("uint8")
-    
+
     # apply gamma correction using the lookup table
     return cv2.LUT(frame, table)
 
@@ -85,7 +85,7 @@ def detect_line_segments(cropped_edges):
     rho = 1  # distance precision in pixel, i.e. 1 pixel
     angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
     min_threshold = 10  # minimal of votes
-    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
                                     np.array([]), minLineLength=15, maxLineGap=12)
 
     return line_segments
@@ -97,7 +97,7 @@ def average_slope_intercept(frame, line_segments):
 
     @param frame: original image from front camera
     @param line_segments: line segments found by detect_line_segments
-    @returns: average of lines found 
+    @returns: average of lines found
     """
     lane_lines = []
     if line_segments is None:
@@ -175,7 +175,7 @@ def show_image(title, frame):
     cv2.waitKey(0) # exit window by pressing any of the arrow keys
 
 def calc_steer_cmd(lane_lines, frame):
-    """ 
+    """
     Find the steering angle based on lane line coordinate
     Modified from: https://github.com/dctian/DeepPiCar/blob/master/driver/code/hand_coded_lane_follower.py
 
@@ -185,7 +185,7 @@ def calc_steer_cmd(lane_lines, frame):
     """
     if len(lane_lines) == 0:
         return 0
-    
+
     height, width, _ = frame.shape
     if len(lane_lines) == 1: # if only one lane line is detected
         x1, y1, x2, y2 = lane_lines[0][0]
@@ -202,13 +202,13 @@ def calc_steer_cmd(lane_lines, frame):
     base_point = (width/2, height)
     x_offset = halfpoint[0] - base_point[0]
     y_offset = halfpoint[1] - base_point[1]
-    
+
     steer_cmd = math.atan2(y_offset, x_offset) + np.pi/2
 
     return steer_cmd
 
 def calc_steer_cmd2(lane_lines, frame):
-    """ 
+    """
     Find the steering angle based on lane line coordinate
     Modified from: https://github.com/dctian/DeepPiCar/blob/master/driver/code/hand_coded_lane_follower.py
 
@@ -218,7 +218,7 @@ def calc_steer_cmd2(lane_lines, frame):
     """
     if len(lane_lines) == 0:
         return 0
-    
+
     height, width, _ = frame.shape
     if len(lane_lines) == 1: # if only one lane line is detected
         print("one line detected")
@@ -247,7 +247,7 @@ def calc_steer_cmd2(lane_lines, frame):
     cv2.circle(frame, halfpoint, 3, (0, 0, 255), 5)
     x_offset = halfpoint[0] - base_point[0]
     y_offset = halfpoint[1] - base_point[1]
-    
+
     # steer_cmd = np.arctan2(x_offset, y_offset)
     steer_cmd = math.atan2(y_offset, x_offset) + np.pi/2
     print("x_offset ", x_offset)
@@ -256,30 +256,35 @@ def calc_steer_cmd2(lane_lines, frame):
 
     return steer_cmd, frame
 
-def get_steer_cmd(frame_, curr_steer_angle):
+def get_steer_cmd(frame_, curr_steer_angle, frame_is_array=False):
     '''
     Function used by Webots simulator to generate steering command given image from front camera
 
     @param frame: image from Tesla Model 3 front camera
     @param curr_steer_angle: vehicle's current steering angle (for signal smoothing purposes)
-    @returns: steering command for vehicle 
+    @returns: steering command for vehicle
     '''
-    frame = cv2.imread(frame_)
+    if not frame_is_array:
+        frame = cv2.imread(frame_)
+    else:
+        frame = frame_
     edges = detect_edges(frame)
-
     cropped_edges = region_of_interest(edges)
-
+    print(np.all(edges==0))
     line_segments = detect_line_segments(cropped_edges)
     line_segment_image = display_lines(frame, line_segments)
 
     lane_lines = average_slope_intercept(frame, line_segments)
     if not lane_lines:
+        print('no lines')
         return curr_steer_angle
     lane_lines_image = display_lines(frame, lane_lines)
 
     if lane_lines:
         steer_cmd = calc_steer_cmd(lane_lines, lane_lines_image)
+        print('lines')
     else:
+        print('no lines')
         steer_cmd = 0
 
     # max_dev = np.pi/6
@@ -324,16 +329,17 @@ def main():
         show_image('calc steer cmd 2', tmp_frame)
     else:
         steer_cmd = 0
-    
+
     max_angle = np.pi/6
     if steer_cmd > max_angle:
         steer_cmd = max_angle
     elif steer_cmd < -max_angle:
         steer_cmd = -max_angle
-    
+
     print("steering command ", steer_cmd)
     print("steering command in degrees ", steer_cmd * 180/np.pi)
+    return steer_cmd
 
 
 if __name__ == '__main__':
-    main()    
+    main()
---
+++ controller/scripts/lane_follower_node.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+
+import rospy
+import numpy as np
+import lane_detector as ld
+from std_msgs.msg import String
+from webots_ros.srv import *
+from webots_ros.msg import Float64Stamped
+#from simple_control.srv import *
+from simple_control.msg import ControlCommand
+from sensor_msgs.msg import Image
+
+class service(object):
+    def __init__(self, path, srv_type):
+        self.path = vehicle_name + path
+        self.type = srv_type
+        rospy.loginfo("Waiting for service %s" %self.path)
+        rospy.wait_for_service(self.path)
+        self.srv = rospy.ServiceProxy(self.path, self.type)
+        rospy.loginfo("Got service %s" %self.path)
+
+def vehicle_name_callback(data):
+    global vehicle_name
+    vehicle_name = data.data
+    rospy.loginfo("Got vehicle name")
+
+def steering_angle_callback(data):
+    global steering_angle
+    steering_angle = data.data
+
+def throttle_callback(data):
+    global throttle
+    throttle = data.data
+
+def image_callback(data):
+    global camera_image
+    camera_image = data.data
+
+def main():
+    global steering_angle, throttle, vehicle_name, camera_image
+
+    while vehicle_name is None:
+        pass
+    rospy.loginfo("Vehicle name: " + vehicle_name)
+
+    # Publishers:
+    control_pub = rospy.Publisher('control_command', ControlCommand, queue_size=10)
+    # Subscribers:
+    rospy.Subscriber(vehicle_name+"/automobile/steering_angle", Float64Stamped, steering_angle_callback)
+    rospy.Subscriber(vehicle_name+"/front_camera/image", Image, image_callback)
+    rospy.Subscriber(vehicle_name+"/automobile/throttle", Float64Stamped, throttle_callback)
+
+    # Services
+    # Control
+    set_throttle = service("/automobile/set_throttle", set_float)
+    set_brake_intensity = service("/automobile/set_brake_intensity", set_float)
+    set_gear = service("/automobile/set_gear", set_int)
+    set_steering_angle = service("/automobile/set_steering_angle", set_float)
+    set_cruising_speed = service("/automobile/set_cruising_speed", set_float)
+    # Misc
+    get_dimensions = service("/automobile/get_dimensions", automobile_get_dimensions)
+    get_time_step = service("/robot/get_basic_time_step", get_float)
+    # Front camera
+    enable_front_camera = service("/front_camera/enable", set_int)
+    enable_recognition = service("/front_camera/recognition_enable", set_int)
+    camera_save_image = service("/front_camera/save_image", save_image)
+    get_info = service("/front_camera/get_info", camera_get_info)
+    # Other sensors
+    enable_rear_camera = service("/rear_camera/enable", set_int)
+    enable_lidar = service("/Sick_LMS_291/enable", set_int)
+    enable_point_cloud = service("/Sick_LMS_291/enable_point_cloud", set_bool)
+    enable_gps = service("/gps/enable", set_int)
+    enable_gyro = service("/gyro/enable", set_int)
+    rospy.loginfo("All services ready")
+
+    # Initial service calls
+    # Misc
+    dims = get_dimensions.srv()
+    TRACK = dims.trackRear
+    BASE = dims.wheelBase
+    WHEEL = dims.frontWheelRadius
+    #timestep = get_time_step.srv()
+    timestep = 10 # hard code if not running webots from launch file
+    # Front camera
+    enable_front_camera.srv(30)
+    enable_recognition.srv(1)
+    info = get_info.srv()
+    camera_width = info.width
+    camera_height = info.height
+    # Other sensors
+    enable_gps.srv(1)
+    enable_gyro.srv(1)
+    enable_lidar.srv(timestep)
+    enable_point_cloud.srv(True)
+    # Control
+    set_brake_intensity.srv(0)
+    set_gear.srv(1)
+    set_throttle.srv(.5)
+
+    while camera_image is None:
+        pass
+    while not rospy.is_shutdown():
+        front_img = np.frombuffer(camera_image, np.uint8).reshape((camera_height, camera_width, 4))
+        # no idea where this saves the image:
+        save_result = camera_save_image.srv(filename="~/front_img1.jpg", quality=75)
+        print(save_result)
+        print('curr steer cmd', steering_angle)
+        #yaw = ld.get_steer_cmd("front_img1.jpg", steering_angle)
+        yaw = ld.get_steer_cmd("~/front_img1.jpg", steering_angle)
+        #yaw = ld.get_steer_cmd(front_img, steering_angle, frame_is_array=True)
+        print("yaw: ", yaw)
+        steering_cmd = (yaw-steering_angle) * 0.525
+        set_steering_angle.srv(steering_cmd)
+
+        cruising_speed = 20
+        set_cruising_speed.srv(cruising_speed)
+        # u = [throttle, brake, desired steering angle, desired gear]
+        u = ControlCommand(throttle=throttle, brake=0, steering_angle=steering_cmd, gear=1)
+        control_pub.publish(u)
+
+camera_image = None
+vehicle_name = None
+steering_angle = 0
+throttle = 0
+rospy.init_node('controller', anonymous=True)
+rospy.Subscriber("model_name", String, vehicle_name_callback)
+main()
